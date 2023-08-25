af9843cf2d74253e98fc6fed7875300e031d0ee1
r0bertinho <112725039+r0bertinho@users.noreply.github.com>
2023-08-25 14:53:25 -0300

    Update cubesat_main.py

--- cubesat_main.py
+++ cubesat_main.py
@@ -1,3 +1,10 @@
 # importing the needed libraries{
+import os
+import time
+# }
 
+# Define the mission parameters{
+mission = os.Mission("C.E.D Miryam Ervilha")
+position = os.Location(lat=37.7749, lon=-122.4194, alt=500)
+velocity = os.Velocity(x=0, y=0, z=5)
 # }
