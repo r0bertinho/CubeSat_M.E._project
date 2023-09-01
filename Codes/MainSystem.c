d796c54a1e58f96ad1113546fd91c980464dd322
Ian Souza Freire <112725039+r0bertinho@users.noreply.github.com>
2023-09-01 14:43:08 -0300

    Update MainSystem.c

--- Codes/MainSystem.c
+++ Codes/MainSystem.c
@@ -1,27 +1,29 @@
+/* Notes:
+- Try your best to keep the code clean;
+*/
 #include <stdio.h>
 
 
-int check_init()        /* check if program will be initated */
+int check_init()        /* Check if program will be initated */
 {
-    int in_mission;        /* 1 for true */
+    int in_mission;        /* 1 for true, 0 for false */
     printf("Are we in mission? (1 for true, 0 for false)  ");
-    scanf("%d", &in_mission);        /* Use & before in_mission to store the input value */
+    scanf("%d", &in_mission);        /* Storing the number given */
 
-    if (in_mission == 0) {return in_mission;}
-    else if (in_mission == 1) {return in_mission;}
+    if (in_mission == 0) {printf("ok, finishing!"); return 0;}        /* if false, terminate */
+    else if (in_mission == 1) {return in_mission;}        /* returning true */
 
-    return 0;
+    return 0;    /* if the input isn't 1 or 0 */
 }
 
 
 int main()
 {
-    int in_mission = check_init();
+    int in_mission = check_init();        /* sees the result returned by check_init() */
 
-    if (in_mission == 1) {printf("started! \n");}
-    else if (in_mission == 0) {printf("ok, finishing!"); return 0;}
+    if (in_mission == 1) {printf("started! \n");}        /* main code goes here, but change it for a loop rhamon! */
     else {return 0;}
 
-    printf("ended!");
-    return 0;
+    printf("ended!");        /* returning false */
+    return 0;        /* in case check_init() isn't started or the program is terminated */
 }
