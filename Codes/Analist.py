97e16380e51f08df41438b0eea00d367cdddae09
Ian Souza Freire <112725039+r0bertinho@users.noreply.github.com>
2023-09-01 13:47:15 -0300

    Update Analist.py

--- Codes/Analist.py
+++ Codes/Analist.py
@@ -5,9 +5,9 @@ import os
 
 # Setting basis
 Dir = os.getcwd()
-Path_to_Shearer = os.path.join(Dir, 'main/Codes/Shearer.h')
-Path_to_C_script = os.path.join(Dir, 'main/Codes/C_script.c')
-Path_to_S_and_R = os.path.join(Dir, 'main/Codes/Sender_and_Receiver.c')
+Path_to_Shearer = os.path.join(Dir, 'main/Codes/Shearer.h') # Path to the shearer
+Path_to_C_script = os.path.join(Dir, 'main/Codes/MainSystem.c') # Path to the main code
+Path_to_S_and_R = os.path.join(Dir, 'main/Codes/Sender_and_Receiver.c') # Path to the receptor
 
 # Getting information from Shearer.h
 Run = int(subprocess.check_output(["in_mission", Path_to_C_script], universal_newlines=True))
