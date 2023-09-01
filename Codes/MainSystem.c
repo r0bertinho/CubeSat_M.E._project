d58bf07cdb13f038504bc25bbb56a6097a2db95a
Ian Souza Freire <112725039+r0bertinho@users.noreply.github.com>
2023-09-01 14:51:02 -0300

    Update MainSystem.c

--- Codes/MainSystem.c
+++ Codes/MainSystem.c
@@ -2,7 +2,7 @@
 - Try your best to keep the code clean
 - Keep it with comments for easy understanding
 - Comments are made this way(the slash and star doens't have a space): / *  * /
-- First letter of comments needs to have Caps Lock unless it is part of the code
+- First letter of comments needs to have Caps Lock unless it is part of the code or a number
 */
 #include <stdio.h>
 
@@ -13,20 +13,20 @@ int check_init()        /* Check if program will be initated */
     printf("Are we in mission? (1 for true, 0 for false)  ");
     scanf("%d", &in_mission);        /* Storing the number given */
 
-    if (in_mission == 0) {printf("ok, finishing!"); return 0;}        /* if false, terminate */
-    else if (in_mission == 1) {return in_mission;}        /* returning true */
+    if (in_mission == 0) {printf("ok, finishing!"); return 0;}        /* If false, terminate */
+    else if (in_mission == 1) {return in_mission;}        /* Returning true */
 
-    return 0;    /* if the input isn't 1 or 0 */
+    return 0;    /* If the input isn't 1 or 0 */
 }
 
 
-int main()
+int main()        /* Main program where the code will be */
 {
-    int in_mission = check_init();        /* sees the result returned by check_init() */
+    int in_mission = check_init();        /* Sees the result returned by check_init() */
 
-    if (in_mission == 1) {printf("started! \n");}        /* main code goes here, but change it for a loop rhamon! */
+    if (in_mission == 1) {printf("started! \n");}        /* Main code goes here, but change it for a loop rhamon! */
     else {return 0;}
 
-    printf("ended!");        /* returning false */
-    return 0;        /* in case check_init() isn't started or the program is terminated */
+    printf("ended!");        /* Returning false */
+    return 0;        /* In case check_init() isn't started or the program is terminated */
 }
