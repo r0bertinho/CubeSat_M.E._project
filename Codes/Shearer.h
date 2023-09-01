afbdbbd55248e4a7cf5c4fc97cad6f5ae85f4190
Ian Souza Freire <112725039+r0bertinho@users.noreply.github.com>
2023-09-01 15:33:25 -0300

    Update Shearer.h

--- Codes/Shearer.h
+++ Codes/Shearer.h
@@ -1,1 +1,7 @@
+/* 
+- This file is for making a bridge for the other files
+- Feel free to get here every variable you think is important
+- Don't forget to put only important things and always revise this file rhamon!
+*/
+
 extern int in_mission;
