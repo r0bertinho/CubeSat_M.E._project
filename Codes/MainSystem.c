ba07727286440659f16324a8c70bd18708103f15
Ian Souza Freire <112725039+r0bertinho@users.noreply.github.com>
2023-09-01 14:33:03 -0300

    Update MainSystem.c

--- Codes/MainSystem.c
+++ Codes/MainSystem.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 
-int check_init()
+int check_init()        /* check if program will be initated */
 {
-    int in_mission;  /* 1 for true */
+    int in_mission;        /* 1 for true */
     printf("Are we in mission? (1 for true, 0 for false)  ");
-    scanf("%d", &in_mission);  /* Use & before in_mission to store the input value */
+    scanf("%d", &in_mission);        /* Use & before in_mission to store the input value */
 
-    return in_mission;
+    if (in_mission == 0) {return in_mission;}
+    else if (in_mission == 1) {return in_mission;}
+
+    return 0;
 }
 
 
@@ -16,6 +19,7 @@ int main()
     int in_mission = check_init();
 
     if (in_mission == 1) {printf("started! \n");}
+    else if (in_mission == 0) {printf("ok, finishing!"); return 0;}
     else {return 0;}
 
     printf("ended!");
