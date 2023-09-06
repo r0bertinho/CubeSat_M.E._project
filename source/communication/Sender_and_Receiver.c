4af1ddbd555ae26118f4401ddf9c6893f8eca961
Ian Souza Freire <112725039+r0bertinho@users.noreply.github.com>
2023-09-06 14:05:20 -0300

    Update Sender_and_Receiver.c

--- source/communication/Sender_and_Receiver.c
+++ source/communication/Sender_and_Receiver.c
@@ -1,25 +1,29 @@
 #include <studio.h>
 #include "Shearer.h"
 
+
 void G_Shearer(*in_mission)
 {
     *in_mission = Shearer.h
 }
 
+
 void m_Sender_n_receiver()
 {
     while (in_mission == 1)
     {
+       
         void communicateWithSatellite()        /* Start by here! Put things like variables, paths, etc... */
         {
             printf("Communicating with satellite...\n");
             printf("Message received from satellite: Hello, Earth!\n");
         }
-            
+
+       
         int m_in_main()        /* This is where we make it actually work! */
         {
             communicateWithSatellite();
-        
+
             return 0;
         }
         return 0;
