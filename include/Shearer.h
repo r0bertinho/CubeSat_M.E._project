858c321cd7bb6ea966ecfb17f2724625e33fde2e
Ian Souza Freire <112725039+r0bertinho@users.noreply.github.com>
2023-09-06 13:41:30 -0300

    Update Shearer.h

--- include/Shearer.h
+++ include/Shearer.h
@@ -6,10 +6,10 @@
 #ifndef Shearer_h
 #define Shearer_h
 
-/* Put variables got from MainSystem.c */
+		/* Put variables got from MainSystem.c */
 extern int in_mission;
 
-/* Put variables got from Sender_and_receiver */
+		/* Put variables got from Sender_and_receiver */
 
 
 #endif
