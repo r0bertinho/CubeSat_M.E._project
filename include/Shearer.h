9b34bfc7332c2541a0de3df6bec26c8639370d83
Ian Souza Freire <112725039+r0bertinho@users.noreply.github.com>
2023-09-07 10:24:48 -0300

    Update Shearer.h

--- include/Shearer.h
+++ include/Shearer.h
@@ -1,4 +1,5 @@
 /* 
+- This file uses  Tabs / 4 / No wrap
 - This file is for making a bridge for the other files
 - Feel free to get here every variable you think is important
 - Don't forget to put only important things and always revise this file rhamon!
