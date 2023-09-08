13aa9238ddb81b0f3890ccaa5e60849a611473b3
Ian Souza Freire <112725039+r0bertinho@users.noreply.github.com>
2023-09-08 17:00:23 -0300

    Update Utils.c

--- source/utilities/Utils.c
+++ source/utilities/Utils.c
@@ -4,7 +4,37 @@
 #include <stdio.h>
 #include "Shearer.h"
 
-while (in_mission == 1)		/* main code here */
+
+int main()		/* Initialize CubeSat hardware and sensors */
 {
-	
+    if (initializeCubeSat() != 0)
+	{
+        printf("Error initializing CubeSat hardware.\n");
+        return 1;
+    }
+
+ 		   /* Main loop for CubeSat operations */
+    while (in_mission == 1)		/* Read sensor data (e.g., temperature, pressure, etc.) */
+	{
+        double temperature = readTemperatureSensor();
+        double pressure = readPressureSensor();
+
+      		  /* Process sensor data and make decisions */
+        if (temperature > THRESHOLD_TEMPERATURE)		/* Implement corrective action here */
+		{
+            printf("Temperature is too high. Taking corrective action...\n");
+        }
+
+        if (pressure < THRESHOLD_PRESSURE)		/* Implement safety protocols here */
+		{
+            printf("Low pressure detected. Initiating safety protocols...\n");
+        }
+
+        controlThrusters();		/* Control CubeSat components (e.g., thrusters, solar panels, etc.) */
+
+        sleep(SLEEP_INTERVAL_SECONDS);		/* Sleep for a while before the next iteration */
+    }
+
+    finalizeCubeSat();		/* Cleanup and finalize CubeSat operations */
+    return 0;
 }
