0cdb82612c0d8b47e643f3ac16b53656c5e86656
Ian Souza Freire <112725039+r0bertinho@users.noreply.github.com>
2023-09-09 14:41:57 -0300

    Update MainSystem.c

--- source/MainSystem.c
+++ source/MainSystem.c
@@ -7,6 +7,7 @@
 - First letter of comments needs to have Caps Lock unless it is part of the code or a number
 */
 #include <stdio.h>
+#include <unistd.h>
 #include "Shearer.h"
 
 
@@ -14,6 +15,9 @@ void check_init(int *in_mission)        /* Check if program will be initated */
 {
     printf("Are we in mission? (1 for true, 0 for false)  ");        /* 1 for true, 0 for false */
     scanf("%d", in_mission);        /* Storing the number given */
+    
+    printf("What will be the sleep time?");
+    scanf("%lf", SLEEP_INTERVAL_SECONDS_U);
 }
 
 void information_base(double *Cs, double *A)        /* Base de dados do stélite para o main() */
@@ -23,8 +27,8 @@ void information_base(double *Cs, double *A)        /* Base de dados do stélite
         A = Altitude;
      */
 
-    Cs = 0.0;
-    A = 0.0;
+    Cs = 0;
+    A = 0;
 }
 
 int main()        /* Main program where the code will be */
@@ -42,7 +46,7 @@ int main()        /* Main program where the code will be */
         
                 /* See if the in_mission is still true by getting the value from Shearer.h */
         in_mission = in_mission;
-        
+        usleep(SLEEP_INTERVAL_SECONDS_U);
     }
 
     printf("ended!");        /* Returning false or ending the program */
