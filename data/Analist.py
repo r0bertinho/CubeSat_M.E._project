9111d3e24dcca1b3c3e81031ecae3d7e53dab6a2
Ian Souza Freire <112725039+r0bertinho@users.noreply.github.com>
2023-09-13 18:48:11 -0300

    Ian 13-09-2023

--- data/Analist.py
+++ data/Analist.py
@@ -1,12 +1,13 @@
 # Importing just what is needed to run the code
 from time import sleep
+from shutil import move, delete
 from subprocess import check_output
 from os.path import join
 from os import getcwd
 
 # Setting basis
 Dir = getcwd()
-Path_to_C_script = join(Dir, 'source/MainSystem.c') # Path to the main code
+Path_to_C_script = join(Dir, 'source/main.c') # Path to the main code
 Path_to_Shearer = join(Dir, 'source/utilities/Shearer.h') # Path to the shearer
 Path_to_Get_Sensor = join(Dir, 'source/sensors/GetSensors.c') # Path to the receptor
 Path_to_S_and_R = join(Dir, 'source/communication/Sender_and_Receiver.c') # Path to the receptor
@@ -19,30 +20,37 @@ Run = int(check_output(["in_mission", Path_to_C_script], universal_newlines=True
 def analise_data(Fnum):
     
     # Opening the file of data
-    open("data" + Fnum + ".txt", "x")
+    open("data_" + Fnum + ".txt", "x")
     
-    with open("data" + Fnum + ".txt", "w") as Df:
-    Df.write(
-        # Here will go the data analisys
+    with open("data_" + Fnum + ".txt", "w") as Df:
+        Df.write(
+          # Here will go the data analisys
         
 
         )
-    Df.close() # Closing the file
+        Df.close() # Closing the file
 
+# Setting variables before the loop
+NumOfFiles = 1
 
 # Loop(put here all of the data analisys)
 while Run == 1:
 
+    # Analysing the data
+    analise_data(NumOfFiles)
+
     # Setting variables
-    Signal = False
     Time_to_wait = 5  # in seconds 1h = 3600
+
+    # Moving the file to the folder "analysed"
+    move("data_" + str(NumOfFiles) + ".txt", "analysed")
 
     # Pausing the code before redoing the loop
     sleep(Time_to_wait)
 
     # Put here something to know the code ended or make the loop redo some times
-    
+    Run = int(check_output(["in_mission", Path_to_C_script], universal_newlines=True))
 
-
-# Here we will export the finished product for the Shearer.h
-
+    # Deleting the file to save SPACE before replacing the analisys number
+    delete("data_" + str(NumOfFiles) + ".txt")
+    NumOfFiles += 1
