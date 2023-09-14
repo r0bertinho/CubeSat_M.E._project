ecf4a1eba1df676a2c169735fd94889c0d946fee
Ian Souza Freire <112725039+r0bertinho@users.noreply.github.com>
2023-09-14 15:37:45 -0300

    Ian 14-09-2023

--- data/Analist.py
+++ data/Analist.py
@@ -1,18 +1,21 @@
 # Importing just what is needed to run the code (2 per line)
-from time import sleep; from shutil import move, delete
+from time import sleep; from shutil import move
 from subprocess import check_output; from os.path import join
 from os import getcwd; from datetime import datetime
+from os import remove;
 
 # Setting basis
 Dir = getcwd()
-Path_to_C_script = join(Dir, 'source/main.c') # Path to the main code
-Path_to_Shearer = join(Dir, 'source/utilities/Shearer.h') # Path to the shearer
-Path_to_Get_Sensor = join(Dir, 'source/sensors/GetSensors.c') # Path to the receptor
-Path_to_S_and_R = join(Dir, 'source/communication/Sender_and_Receiver.c') # Path to the receptor
+Path_to_C_script = join(Dir, "source/main.c") # Path to the main code
+Path_to_Shearer = join(Dir, "include/Shearer.h") # Path to the shearer
+Path_to_Get_Sensor = join(Dir, "source/sensors/GetSensors.c") # Path to the receptor
+Path_to_S_and_R = join(Dir, "source/communication/Sender_and_Receiver.c") # Path to the receptor
 
 # Getting information from Shearer.h
 RunIN = int(check_output(["in_mission", Path_to_C_script], universal_newlines=True))
 
+# Defining variables before loop
+Time_to_wait = 3600.0  # in seconds 1h = 3600
 
 #Here will go the data analisys and generating a file with it
 class analise_data:
@@ -35,6 +38,7 @@ class analise_data:
 # Setting variables before the loop
 NumOfFiles = 1
 
+
 # Loop(put here all of the data analisys)
 while RunIN == 1:
 
@@ -48,23 +52,28 @@ while RunIN == 1:
     # Analysing the data
     analise_data.create_file(str(NumOfFiles))
     analise_data.write_data(
-        time=(str(minutes) + " " 
-        + str(hour) + " " + str(day) + "-" 
-        + str(month) + "-" + str(year)
-    ))
+        time=(
+            str(minutes) + ":" 
+            + str(hour) + "  " 
+            + str(day) + "-" 
+            + str(month) + "-" 
+            + str(year)
+        )
+    )
 
     # Setting variables
-    Time_to_wait = 5  # in seconds 1h = 3600
+    folder_for_data = join(Dir, "data/analysed")
+    current_file = "data_" + str(NumOfFiles) + ".txt"
 
     # Moving the file to the folder "analysed"
-    move("data_" + str(NumOfFiles) + ".txt", "analysed")
+    move(current_file, "analysed")
 
     # Pausing the code before redoing the loop
-    sleep(Time_to_wait)
+    sleep(secs=(Time_to_wait * 24)) # Transforming 1 hour to 1 day
 
     # Put here something to know the code ended or make the loop redo some times
     RunIN = int(check_output(["in_mission", Path_to_C_script], universal_newlines=True))
 
     # Deleting the file to save SPACE before replacing the analisys number
-    delete("data_" + str(NumOfFiles) + ".txt")
+    remove(path=dir, )
     NumOfFiles += 1
