aa38af9e22534d16ec01c11fc7cf752dc678ec82
Ian Souza Freire <112725039+r0bertinho@users.noreply.github.com>
2023-11-02 20:24:36 -0300

    Correct "Base.h"

--- TB_build/include/Base.h
+++ TB_build/include/Base.h
@@ -3,9 +3,9 @@
 #ifndef Base_h
 #define Base_h
 
-#define extern int in_mission;
-#define extern int SLEEP_INTERVAL_SECONDS_U;
-#define extern int SLEEP_INTERVAL_SECONDS;
+// Zero means the value is defined in another file
+extern int in_mission = 0; // main.c
+extern int SLEEP_INTERVAL_SECONDS_U = 0; // main.c
 
 #define try bool __HadError=false;
 #define catch(x) ExitJmp:if(__HadError)
